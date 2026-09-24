ea9cb58302ec37364f1c98fb7d24a236d5a69717
SaiNageswari16 <sainageswari16@gmail.com>
2026-09-24 23:57:10 +0530

    error

--- backend/src/api/deps.py
+++ backend/src/api/deps.py
@@ -3,7 +3,7 @@ from typing import Annotated
 
 from fastapi import Depends, HTTPException, Request, status
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
-from sqlalchemy import select
+from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
